18bfe5e354ab5cfbd7a60ed41c36aae0c845172e
dsh91698 <mettus@mail.ru>
2022-11-04 04:40:00 +0400

    refactor: chage to material components

--- catsapi/src/app/app.module.ts
+++ catsapi/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { OneKittyComponent } from './one-kitty/one-kitty.component';
 import { HttpClientModule } from '@angular/common/http';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from "@angular/material/card";
 
 
 @NgModule({
@@ -18,6 +20,8 @@ import { HttpClientModule } from '@angular/common/http';
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    MatButtonModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent],

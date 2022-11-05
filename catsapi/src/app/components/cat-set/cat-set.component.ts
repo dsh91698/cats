b2cf68d26cb7f5844486d4ceaaba88c4a72b395c
dsh91698 <mettus@mail.ru>
2022-11-05 06:15:47 +0400

    feat: search for breed add

--- catsapi/src/app/components/cat-set/cat-set.component.ts
+++ catsapi/src/app/components/cat-set/cat-set.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { debounceTime } from 'rxjs/operators';
 import { BreedService } from 'src/app/services/breed.service';
 
 @Component({
@@ -11,8 +13,11 @@ export class CatSetComponent implements OnInit {
 
   public form!: FormGroup ;
 
+  public allBreedsObj: any[] = [];
+
   constructor(
     public breedService: BreedService,
+    public router: Router,
   ) {
     this.form = new FormGroup({});
   }
@@ -29,6 +34,28 @@ export class CatSetComponent implements OnInit {
       ]),
     });
 
+    this.breedService.getAllBreeds().subscribe( // load all the breeds in component variable
+      (breedsResponse: any[]) => {
+        console.log('breeds all->', breedsResponse[0].name);
+        this.allBreedsObj = breedsResponse;
+      },
+    );
+
+
+    this.breedService.breedInput.valueChanges.pipe(
+      debounceTime(1500),
+    ).subscribe(
+      searchPhrase => {
+        console.log('breed input->', searchPhrase);
+        const foundBreed = this.breedService.findBreedIds(searchPhrase, this.allBreedsObj);
+        console.log('found breed->', foundBreed);
+        this.breedService.breed = foundBreed.id;
+        this.breedService.getCatsPhotoSet();
+
+      });
+
   }
+
+
 
 }
--- catsapi/src/app/services/breed.service.ts
+++ catsapi/src/app/services/breed.service.ts
@@ -2,12 +2,16 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { IOneKitty } from '../interfaces/IOneKitty';
+import { FormControl } from '@angular/forms';
 
 
 @Injectable({
   providedIn: 'root',
 })
 export class BreedService {
+
+  public breedInput = new FormControl();
+
 
   public searchLimit = 6;
 
@@ -19,14 +23,22 @@ export class BreedService {
     private http: HttpClient,
   ) {}
 
-  // public getAllBreeds(): Observable<any> {
-  // return this.http.get(`https://api.thecatapi.com/v1/images/search?limit=${searchLimit}&breed_ids=${breed}`, {
-  //   headers: {
-  //     'x-api-key':
-  //       'live_4wKIZj63d0cfWTAKiJDaQLQosdxjE8s8gDEFQaOOqnDDRaOrWDvtOBgUlGGK2hkO',
-  //   },
-  // });
-  // }
+  public getAllBreeds(): Observable<any> {
+    return this.http.get('https://api.thecatapi.com/v1/breeds',
+    );
+  }
+
+
+  public findBreedIds(
+    searchString: string,
+    breedsArr: any[]) {
+
+    const findBreeds = breedsArr.filter(a =>
+      a.name.toLowerCase().includes(searchString.toLowerCase())
+          || a.id.includes(searchString.toLowerCase()));
+
+    return  findBreeds[0]; // final
+  }
 
   public getCatPhotos(): Observable<any> {
     if (this.breed) {
@@ -35,5 +47,13 @@ export class BreedService {
       return this.http.get(`https://api.thecatapi.com/v1/images/search?limit=${this.searchLimit}&has_breeds=1`);
     }
   }
+
+  public getCatsPhotoSet(): void {
+    let cats = this.getCatPhotos();
+    cats.subscribe((respond: IOneKitty[]) => {
+      this.setOfCats = respond;
+    });
+  }
+
 
 }
